5e7517ec0a1c55c63922f794a0ee387feeb35c8b
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-03-15 09:46:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function stringChop(str, size) {
   // your code here
+	let x = 0
+	let arr = []
+	arr.pop(str.slice(x,x+size))
+	x = x +size
+	if(x>str.length){
+		break
+	}
 }
 
 // Do not change the code below
